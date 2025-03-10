bdd246bcfae3792e3ed0be822ff203e53d3c34d6
Heithor <johanefelizu@gmail.com>
2025-03-10 17:16:04 -0400

    Added database connection string

--- obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
+++ obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad48c75c3e994b559c31940393ea51d372d097cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
