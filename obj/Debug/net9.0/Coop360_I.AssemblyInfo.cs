23beef9ce28ef8266e20adb328acc177337f7f3f
unknown <johanefelizu@gmail.com>
2025-03-13 11:16:18 -0400

    Starting the dashboard structure

--- obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
+++ obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c7015d7397f39b3807a6e9ef0b8961a24454c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cef521cb0f8af96cf4b93a12ed204f9b51e68e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
