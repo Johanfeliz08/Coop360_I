61da6fcefd1d3aee1999c0765ff96085447480ec
Heithor <johanefelizu@gmail.com>
2025-03-20 17:12:43 -0400

    Added prestamos files and empleados files base - Designing tables, action buttons done

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Coop360_I.Models;
+using Coop360_I.Data;
+
+namespace Coop360_I.Controllers;
+
+public class EmpleadosController : Controller {
+    private readonly ILogger<EmpleadosController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public EmpleadosController(ApplicationDbContext context, ILogger<EmpleadosController> logger) {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult RegistroEmpleados() {
+        return View();
+    }
+
+    // [HttpGet]
+    // public IActionResult Create() {
+    //     return View();
+    // }
+
+    // [HttpPost]
+    // public IActionResult Create(Empleado empleado) {
+    //     if (ModelState.IsValid) {
+    //         _context.Empleados.Add(empleado);
+    //         _context.SaveChanges();
+    //         return RedirectToAction("Index");
+    //     }
+    //     return View(empleado);
+    // }
+
+    // [HttpGet]
+    // public IActionResult Edit(int id) {
+    //     var empleado = _context.Empleados.Find(id);
+    //     return View(empleado);
+    // }
+
+    // [HttpPost]
+    // public IActionResult Edit(Empleado empleado) {
+    //     if (ModelState.IsValid) {
+    //         _context.Empleados.Update(empleado);
+    //         _context.SaveChanges();
+    //         return RedirectToAction("Index");
+    //     }
+    //     return View(empleado);
+    // }
+
+    // [HttpGet]
+    // public IActionResult Delete(int id) {
+    //     var empleado = _context.Empleados.Find(id);
+    //     return View(empleado);
+    // }
+
+    // [HttpPost]
+    // public IActionResult Delete(Empleado empleado) {
+    //     _context.Empleados.Remove(empleado);
+    //     _context.SaveChanges();
+    //     return RedirectToAction("Index");
+    // }
+}
---
+++ Controllers/PrestamosController.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Coop360_I.Models;
+using Coop360_I.Data;
+
+namespace Coop360_I.Controllers;
+
+public class PrestamosController : Controller
+{
+    private readonly ILogger<PrestamosController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public PrestamosController(ApplicationDbContext context, ILogger<PrestamosController> logger)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult SolicitudPrestamo()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
