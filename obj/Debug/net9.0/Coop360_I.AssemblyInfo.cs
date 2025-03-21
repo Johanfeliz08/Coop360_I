377005d4b4123f8ff069db1c536c3391497fac31
Heithor <johanefelizu@gmail.com>
2025-03-21 13:20:51 -0400

    Added informacion personal and contacto grids

--- obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
+++ obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb7cd4bec105b5a84eec754bf536160ffb38fba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7ea78682589da7fc0b8741698f366f63ea4420")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
