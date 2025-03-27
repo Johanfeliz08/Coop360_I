1ad0e2149e7e883fced4941d0a085577d4359655
Heithor <johanefelizu@gmail.com>
2025-03-27 02:22:28 -0400

    General files

--- obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
+++ obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c439cffb751d63ae1ed5f19408bf6a3388f783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6825389cc403f856953dcd704aaa3709be32beb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
