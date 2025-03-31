0c1a86d5228e1360b8d9bc46e5cf82781eebbb16
unknown <johanefelizu@gmail.com>
2025-03-30 20:52:27 -0400

    Fixed validatino on puestos

--- Controllers/Empleados/PuestosController.cs
+++ Controllers/Empleados/PuestosController.cs
@@ -20,7 +20,7 @@ public class PuestosController : Controller {
             throw new Exception("El servidor no puede procesar la solicitud, objeto puesto vacio");
         }
 
-        if (puesto.ID_PUESTO <= 0 || puesto.NOMBRE == null || puesto.ID_DEPARTAMENTO <= 0) {
+        if (puesto.NOMBRE == null || puesto.SALARIO <= 0 || puesto.ID_DEPARTAMENTO <= 0) {
             throw new Exception("El servidor no puede procesar la solicitud, campos obligatorios vacios");
         }
 
@@ -78,9 +78,13 @@ public class PuestosController : Controller {
             TempData["Error"] = "Ha ocurrido un error al cargar los datos necesarios para la pantalla";
             return RedirectToAction("RegistroPuestos");
         }
+
+        var ViewModel = new PuestoViewModel {
+            Departamentos = departamentos
+        };
         
         ViewBag.Title = "Registro de puesto";
-        return View("~/Views/Empleados/Puestos/FormPuestos.cshtml",departamentos);
+        return View("~/Views/Empleados/Puestos/FormPuestos.cshtml",ViewModel);
     }
 
     // View con formulario para editar empleado
@@ -135,6 +139,11 @@ public class PuestosController : Controller {
     [HttpGet]
 
     public IActionResult Eliminar(string IdPuesto) {
+
+        // Obtiene el CODIGO_EMPLEADO del usuario logeado y redirige al login si no hay un usuario logeado
+        if (HttpContext.Session.GetInt32("ID_USUARIO") == null) {
+                return RedirectToAction("Login", "Auth");
+        } 
             
         if (IdPuesto != null ){
 
@@ -155,83 +164,68 @@ public class PuestosController : Controller {
 
      // Guardar
 
-    // [HttpPost]
-    // public async Task<IActionResult> GuardarAsync(Puesto puesto) {
+    [HttpPost]
+    public async Task<IActionResult> GuardarAsync(Puesto puesto) {
 
-    //     // Obtiene el CODIGO_EMPLEADO del usuario logeado y redirige al login si no hay un usuario logeado
-    //     if (HttpContext.Session.GetInt32("ID_USUARIO") == null) {
-    //             return RedirectToAction("Login", "Auth");
-    //     } 
+        // Obtiene el CODIGO_EMPLEADO del usuario logeado y redirige al login si no hay un usuario logeado
+        if (HttpContext.Session.GetInt32("ID_USUARIO") == null) {
+                return RedirectToAction("Login", "Auth");
+        } 
 
-    //     // try {
-    //     // await _context.Database.ExecuteSqlRawAsync(
-    //     //     "EXEC SP_CREAR_EMPLEADO @CEDULA = {0}, @P_NOMBRE = {1}, @S_NOMBRE = {2}, @P_APELLIDO = {3}, @S_APELLIDO = {4}, @DIRECCION = {5}, @ID_SECTOR = {6}, @ID_CIUDAD = {7}, @ID_PROVINCIA = {8}, @PAIS_NACIMIENTO = {9}, @TELEFONO_PRINCIPAL = {10}, @TELEFONO_SECUNDARIO = {11}, @FECHA_NACIMIENTO = {12}, @EMAIL = {13}, @SEXO = {14}, @ESTADO_CIVIL = {15}, @FRECUENCIA_COBRO = {16}, @CUENTA_BANCO = {17}, @ID_ENTIDAD_BANCARIA = {18}, @ID_PUESTO = {19}, @ID_DEPARTAMENTO = {20}, @TIPO_SANGRE = {21}, @NOMBRE_FAMILIAR_PRIMARIO = {22}, @TELEFONO_FAMILIAR_PRIMARIO = {23}, @PARENTESCO_FAMILIAR_PRIMARIO = {24}, @NOMBRE_FAMILIAR_SECUNDARIO = {25}, @TELEFONO_FAMILIAR_SECUNDARIO = {26}, @PARENTESCO_FAMILIAR_SECUNDARIO = {27}, @ID_NIVEL_APROBACION = {28}, @ESTATUS = {29}, @CREADO_POR = {30}",
-    //     //     empleadoValido.CEDULA, empleadoValido.P_NOMBRE, empleadoValido.S_NOMBRE ?? "", empleadoValido.P_APELLIDO, empleadoValido.S_APELLIDO ?? "",
-    //     //     empleadoValido.DIRECCION, empleadoValido.ID_SECTOR ?? 0, empleadoValido.ID_CIUDAD ?? 0, empleadoValido.ID_PROVINCIA ?? 0, empleadoValido.PAIS_NACIMIENTO,
-    //     //     empleadoValido.TELEFONO_PRINCIPAL, empleadoValido.TELEFONO_SECUNDARIO ?? "", empleadoValido.FECHA_NACIMIENTO, empleadoValido.EMAIL ?? "",
-    //     //     empleadoValido.SEXO, empleadoValido.ESTADO_CIVIL, empleadoValido.FRECUENCIA_COBRO, empleadoValido.CUENTA_BANCO,
-    //     //     empleadoValido.ID_ENTIDAD_BANCARIA ?? 0, empleadoValido.ID_PUESTO ?? 0, empleadoValido.ID_DEPARTAMENTO ?? 0, empleadoValido.TIPO_SANGRE,
-    //     //     empleadoValido.NOMBRE_FAMILIAR_PRIMARIO ?? "", empleadoValido.TELEFONO_FAMILIAR_PRIMARIO ?? "", empleadoValido.PARENTESCO_FAMILIAR_PRIMARIO ?? "",
-    //     //     empleadoValido.NOMBRE_FAMILIAR_SECUNDARIO ?? "", empleadoValido.TELEFONO_FAMILIAR_SECUNDARIO ?? "", empleadoValido.PARENTESCO_FAMILIAR_SECUNDARIO ?? "",
-    //     //     empleadoValido.ID_NIVEL_APROBACION, empleadoValido.ESTATUS, empleadoValido.CREADO_POR ?? 0
-    //     // );
-    //     // } catch (Exception ex) {
-    //     //     TempData["openModal"] = true;
-    //     //     TempData["Error"] = "Ha ocurrido un error al guardar el empleado:";
-    //     //     Console.WriteLine("Error al guardar el registro: " + ex.Message + ex.Source); // Mensaje para el log en el server
-    //     //     return RedirectToAction("RegistroEmpleados");
-    //     //     // throw new Exception("Error al guardar el registro: " + ex.Message + ex.Source);
-    //     // }
+        // Valida el puesto
 
-    //     // // ViewBag.Sucess = "Registro guardado con exito";
-    //     // TempData["openModal"] = true;
-    //     // TempData["Success"] = "El empleado ha sido registrado correctamente.";
-    //     // Console.WriteLine("Empleado guardado con exito"); // Mensaje para el log en el server
-    //     // return RedirectToAction("RegistroEmpleados");
+        var puestoValido = validarPuesto(puesto);
+
+        try {
+         await _context.Database.ExecuteSqlRawAsync(
+            "EXEC SP_CREAR_PUESTO @NOMBRE = {0}, @SALARIO = {1}, @ID_DEPARTAMENTO = {2}",
+            puestoValido.NOMBRE, puestoValido.SALARIO, puestoValido.ID_DEPARTAMENTO ?? 0
+        );
+        } catch (Exception ex) {
+            TempData["openModal"] = true;
+            TempData["Error"] = "Ha ocurrido un error al guardar el puesto:";
+            Console.WriteLine("Error al guardar el registro: " + ex.Message + ex.Source); // Mensaje para el log en el server
+            return RedirectToAction("RegistroPuestos");
+        }
+
+        TempData["openModal"] = true;
+        TempData["Success"] = "El puesto ha sido registrado correctamente.";
+        Console.WriteLine("Puesto guardado con exito"); // Mensaje para el log en el server
+        return RedirectToAction("RegistroPuestos");
         
+        }
+
+
+    // Actualizar empleado
+    [HttpPost]
+    public async Task<IActionResult> ActualizarAsync(Puesto puesto) {
         
-    //     }
+        // Obtiene el CODIGO_EMPLEADO del usuario logeado y redirige al login si no hay un usuario logeado
+        if (HttpContext.Session.GetInt32("ID_USUARIO") == null) {
+            return RedirectToAction("Login", "Auth");
+        }
 
+        // Valida el puesto y devuelve un objeto con los campos validados
+        var puestoValido = validarPuesto(puesto);
 
-    // // Actualizar empleado
-    // [HttpPost]
-    // public async Task<IActionResult> ActualizarAsync(Empleado empleado) {
-        
-    //     // Obtiene el CODIGO_EMPLEADO del usuario logeado y redirige al login si no hay un usuario logeado
-    //     if (HttpContext.Session.GetInt32("ID_USUARIO") == null) {
-    //         return RedirectToAction("Login", "Auth");
-    //     }
+        try {
+         await _context.Database.ExecuteSqlRawAsync(
+            "EXEC SP_ACTUALIZAR_PUESTO @ID_PUESTO = {0}, @NOMBRE = {1}, @SALARIO = {2}, @ID_DEPARTAMENTO = {3}",
+            puestoValido.ID_PUESTO, puestoValido.NOMBRE, puestoValido.SALARIO, puestoValido.ID_DEPARTAMENTO ?? 0
+        );
+        } catch (Exception ex) {
+            TempData["openModal"] = true;
+            TempData["Error"] = "Ha ocurrido un error al actualizar el puesto:";
+            Console.WriteLine("Error al actualizar el registro: " + ex.Message + ex.Source); // Mensaje para el log en el server
+            return RedirectToAction("RegistroPuestos");
+        }
 
-    //     // // Valida el empleado y devuelve un objeto con los campos validados
-    //     // var empleadoValido = validarEmpleado(empleado);
+        TempData["openModal"] = true;
+        TempData["Success"] = "El puesto ha sido actualizado correctamente.";
+        Console.WriteLine("Puesto actualizado con exito"); // Mensaje para el log en el server
+        return RedirectToAction("RegistroPuestos");
 
-    //     // try {
-
-    //     //     await _context.Database.ExecuteSqlRawAsync("EXEC SP_ACTUALIZAR_EMPLEADO @CODIGO_EMPLEADO = {0}, @CEDULA = {1}, @P_NOMBRE = {2}, @S_NOMBRE = {3}, @P_APELLIDO = {4}, @S_APELLIDO = {5}, @DIRECCION = {6}, @ID_SECTOR = {7}, @ID_CIUDAD = {8}, @ID_PROVINCIA = {9}, @PAIS_NACIMIENTO = {10}, @TELEFONO_PRINCIPAL = {11}, @TELEFONO_SECUNDARIO = {12}, @FECHA_NACIMIENTO = {13}, @EMAIL = {14}, @SEXO = {15}, @ESTADO_CIVIL = {16}, @FRECUENCIA_COBRO = {17}, @CUENTA_BANCO = {18}, @ID_ENTIDAD_BANCARIA = {19}, @ID_PUESTO = {20}, @ID_DEPARTAMENTO = {21}, @TIPO_SANGRE = {22}, @NOMBRE_FAMILIAR_PRIMARIO = {23}, @TELEFONO_FAMILIAR_PRIMARIO = {24}, @PARENTESCO_FAMILIAR_PRIMARIO = {25}, @NOMBRE_FAMILIAR_SECUNDARIO = {26}, @TELEFONO_FAMILIAR_SECUNDARIO = {27}, @PARENTESCO_FAMILIAR_SECUNDARIO = {28}, @ID_NIVEL_APROBACION = {29}, @ESTATUS = {30}",
-    //     //     empleadoValido.CODIGO_EMPLEADO,empleadoValido.CEDULA, empleadoValido.P_NOMBRE, empleadoValido.S_NOMBRE ?? "", empleadoValido.P_APELLIDO, empleadoValido.S_APELLIDO ?? "",
-    //     //     empleadoValido.DIRECCION, empleadoValido.ID_SECTOR ?? 0, empleadoValido.ID_CIUDAD ?? 0, empleadoValido.ID_PROVINCIA ?? 0, empleadoValido.PAIS_NACIMIENTO,
-    //     //     empleadoValido.TELEFONO_PRINCIPAL, empleadoValido.TELEFONO_SECUNDARIO ?? "", empleadoValido.FECHA_NACIMIENTO, empleadoValido.EMAIL ?? "",
-    //     //     empleadoValido.SEXO, empleadoValido.ESTADO_CIVIL, empleadoValido.FRECUENCIA_COBRO, empleadoValido.CUENTA_BANCO,
-    //     //     empleadoValido.ID_ENTIDAD_BANCARIA ?? 0, empleadoValido.ID_PUESTO ?? 0, empleadoValido.ID_DEPARTAMENTO ?? 0, empleadoValido.TIPO_SANGRE,
-    //     //     empleadoValido.NOMBRE_FAMILIAR_PRIMARIO ?? "", empleadoValido.TELEFONO_FAMILIAR_PRIMARIO ?? "", empleadoValido.PARENTESCO_FAMILIAR_PRIMARIO ?? "",
-    //     //     empleadoValido.NOMBRE_FAMILIAR_SECUNDARIO ?? "", empleadoValido.TELEFONO_FAMILIAR_SECUNDARIO ?? "", empleadoValido.PARENTESCO_FAMILIAR_SECUNDARIO ?? "",
-    //     //     empleadoValido.ID_NIVEL_APROBACION, empleadoValido.ESTATUS);
-
-    //     // } catch(Exception ex) {
-    //     //     TempData["openModal"] = true;
-    //     //     TempData["Error"] = "Ha ocurrido un error al actualizar el empleado";
-    //     //     Console.WriteLine("Error al actualizar el empleado" + ex.Message + ex.Source); // Mensaje para el log en el server
-    //     //     return RedirectToAction("RegistroEmpleados");
-            
-    //     // }
-        
-    //     //     // ViewBag.Sucess = "Registro actualizado con exito";    
-    //     //     TempData["openModal"] = true;
-    //     //     TempData["Success"] = "El empleado ha sido actualizado correctamente."; 
-    //     //     Console.WriteLine("Empleado actualizado con exito"); // Mensaje para el log en el server
-    //     //     return RedirectToAction("RegistroEmpleados");
-
-    // }
+    }
 
      // Eliminar empleado
 
--- obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
+++ obj/Debug/net9.0/Coop360_I.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b27cea8a396a0bcfb042b226d1dd8262e79553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbc34caaf9f80200b9b75d93940d46db359d8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coop360_I")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
